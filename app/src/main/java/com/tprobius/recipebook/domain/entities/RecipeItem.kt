17b38d820074ad5e5c0f1f0d28cc11989deb0966
Заремба Виктория <dviktoriyas@gmail.com>
2023-10-18 22:10:43 +0700

    Fixed primary key

--- app/src/main/java/com/tprobius/recipebook/domain/entities/RecipeItem.kt
+++ app/src/main/java/com/tprobius/recipebook/domain/entities/RecipeItem.kt
@@ -1,6 +1,5 @@
 package com.tprobius.recipebook.domain.entities
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
@@ -8,27 +7,16 @@ import java.io.Serializable
 @Entity(tableName = "recipes")
 data class RecipeItem(
     @PrimaryKey(autoGenerate = true)
-    val idLocal: Long = -1,
-    @ColumnInfo
+    val idLocal: Long = 0,
     val idRemote: String?,
-    @ColumnInfo
     val name: String?,
-    @ColumnInfo
     val headline: String?,
-    @ColumnInfo
     val description: String?,
-    @ColumnInfo
     val calories: String?,
-    @ColumnInfo
     val proteins: String?,
-    @ColumnInfo
     val fats: String?,
-    @ColumnInfo
     val carbons: String?,
-    @ColumnInfo
     val difficulty: Int?,
-    @ColumnInfo
-    val time: String?,
-    @ColumnInfo
+    val time: Int?,
     val image: String?
 ) : ListItem, Serializable
