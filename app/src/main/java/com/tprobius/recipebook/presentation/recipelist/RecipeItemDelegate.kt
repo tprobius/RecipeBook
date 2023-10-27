488fdb9c1ff983591b5eade9d0beed671bde8e92
Заремба Виктория <dviktoriyas@gmail.com>
2023-10-27 22:56:55 +0700

    Deleted data binding

--- app/src/main/java/com/tprobius/recipebook/presentation/recipelist/RecipeItemDelegate.kt
+++ app/src/main/java/com/tprobius/recipebook/presentation/recipelist/RecipeItemDelegate.kt
@@ -4,6 +4,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.MultiTransformation
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
+import com.tprobius.recipebook.R
 import com.tprobius.recipebook.databinding.ItemRecipeListBinding
 import com.tprobius.recipebook.domain.entities.ListItem
 import com.tprobius.recipebook.domain.entities.RecipeItem
@@ -16,15 +17,32 @@ fun recipeItemDelegate(onRecipeClick: (RecipeItem) -> Unit) =
         }
     ) {
         bind {
-            binding.recipeItem = item
-            binding.imageView
-            binding.cardView.setOnClickListener {
-                onRecipeClick(item)
-            }
+            with(binding) {
+                nameTextView.text = item.name
+                headlineTextView.text = item.headline
+                timeTextView.text = item.time?.let { time -> getString(R.string.time, time) }
+                caloriesTextView.text =
+                    if (item.calories.isNullOrEmpty()) {
+                        getString(R.string.no_data)
+                    } else {
+                        item.calories
+                    }
+                difficultyTextView.text = when (item.difficulty) {
+                    null -> getString(R.string.no_data)
+                    0, 1 -> getString(R.string.easy)
+                    2 -> getString(R.string.medium)
+                    else -> getString(R.string.hard)
+                }
 
-            Glide.with(binding.imageView)
-                .load(item.image)
-                .transform(MultiTransformation(FillSpace(), RoundedCorners(8)))
-                .into(binding.imageView)
+                imageView
+                cardView.setOnClickListener {
+                    onRecipeClick(item)
+                }
+
+                Glide.with(imageView)
+                    .load(item.image)
+                    .transform(MultiTransformation(FillSpace(), RoundedCorners(8)))
+                    .into(imageView)
+            }
         }
     }
