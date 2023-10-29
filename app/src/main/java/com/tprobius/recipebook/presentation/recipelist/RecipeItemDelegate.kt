0eb374940b787aefcbe42d8edd95a4fa912da683
Заремба Виктория <dviktoriyas@gmail.com>
2023-10-29 22:09:15 +0700

    Fixed imageView loading

--- app/src/main/java/com/tprobius/recipebook/presentation/recipelist/RecipeItemDelegate.kt
+++ app/src/main/java/com/tprobius/recipebook/presentation/recipelist/RecipeItemDelegate.kt
@@ -1,14 +1,17 @@
 package com.tprobius.recipebook.presentation.recipelist
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
 import com.tprobius.recipebook.R
 import com.tprobius.recipebook.databinding.ItemRecipeListBinding
 import com.tprobius.recipebook.domain.entities.ListItem
 import com.tprobius.recipebook.domain.entities.RecipeItem
-import com.tprobius.recipebook.presentation.utils.FillSpace
 
 object RecipeItemDelegate {
     fun recipeItemDelegate(onRecipeClick: (RecipeItem) -> Unit) =
@@ -40,10 +43,21 @@ object RecipeItemDelegate {
                         onRecipeClick(item)
                     }
 
-                    Glide.with(imageView)
+                    Glide.with(imageView.context)
+                        .asBitmap()
                         .load(item.image)
-                        .transform(MultiTransformation(FillSpace(), RoundedCorners(8)))
-                        .into(imageView)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .transform(RoundedCorners(8))
+                        .into(object : CustomTarget<Bitmap?>() {
+                            override fun onResourceReady(
+                                resource: Bitmap,
+                                transition: Transition<in Bitmap?>?
+                            ) {
+                                imageView.setImageBitmap(resource)
+                            }
+
+                            override fun onLoadCleared(placeholder: Drawable?) {}
+                        })
                 }
             }
         }
